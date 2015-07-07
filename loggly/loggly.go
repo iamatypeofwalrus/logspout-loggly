d0c4dfba784c2c49735507ee1cc7d07820eec809
Joe Feeney <joefeeney@me.com>
2015-07-07 16:57:48 -0700

    add route via route.Routes.Add in init stage

--- loggly/loggly.go
+++ loggly/loggly.go
@@ -13,22 +13,34 @@ import (
 )
 
 const (
+	adapterName         = "loggly"
 	logglyTokenEnvVar   = "LOGGLY_TOKEN"
 	logglyAddr          = "https://logs-01.loggly.com"
 	logglyEventEndpoint = "/inputs"
 )
 
 func init() {
-	router.AdapterFactories.Register(NewLogglyAdapter, "loggly")
+	fmt.Println("Registering loggly adapter")
+	router.AdapterFactories.Register(NewLogglyAdapter, adapterName)
+
+	r := &router.Route{
+		Adapter: "loggly",
+	}
+
+	err := router.Routes.Add(r)
+	if err != nil {
+		log.Fatal("Could not add route: ", err.Error())
+	}
 }
 
 // NewLogglyAdapter returns an Adapter with that uses a loggly token taken from
 // the LOGGLY_TOKEN environment variable
 func NewLogglyAdapter(route *router.Route) (router.LogAdapter, error) {
+	fmt.Println("Creating Adapter")
 	token := os.Getenv(logglyTokenEnvVar)
 
 	if token == "" {
-		log.Fatal("Could not find environment variable LOGGLY_TOKEN")
+		fmt.Println("Could not find environment variable LOGGLY_TOKEN")
 		return nil, errors.New("")
 	}
 
@@ -50,7 +62,7 @@ type Adapter struct {
 // Stream satisfies the router.LogAdapter interface and passes all logs to Loggly
 func (l *Adapter) Stream(logstream chan *router.Message) {
 	for m := range logstream {
-		log.Println("Received message from stream")
+		fmt.Println("Received message from stream")
 		msg := logglyMessage{
 			Message:           m.Data,
 			ContainerName:     m.Container.Name,
@@ -69,17 +81,16 @@ func (l *Adapter) Stream(logstream chan *router.Message) {
 		req, err := http.NewRequest("POST", url, bytes.NewBuffer(js))
 
 		if err != nil {
-			log.Fatal(err.Error())
+			fmt.Println(err.Error())
 		}
 
-		log.Println("Sending request to loggly")
+		fmt.Println("Sending request to loggly")
 		_, err = l.client.Do(req)
 
 		if err != nil {
 			errMsg := fmt.Sprintf("Error from client: %s", err.Error())
-			log.Fatal(errMsg)
+			fmt.Println(errMsg)
 		}
-
 	}
 }
 

acab27f0cce5816a98ab88d9b578fd5a21b94045
Trevor Bossert <trevor@vurb.com>
2015-09-28 09:47:24 -0700

    some print statements

--- loggly/loggly.go
+++ loggly/loggly.go
@@ -87,10 +87,12 @@ func (l *Adapter) SendMessage(msg logglyMessage) error {
 		return err
 	}
 
+  os.Stdout.Write(logglyTagsEnvVar)
 	url := fmt.Sprintf("%s%s/%s", logglyAddr, logglyEventEndpoint, l.token, "tag", os.Getenv(logglyTagsEnvVar))
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(js))
 
 	req.Header.Add("X-LOGGLY-TAG", os.Getenv(logglyTagsEnvVar))
+	os.Stdout.Write(req.Header)
 
 	if err != nil {
 		return err
